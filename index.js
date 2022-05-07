9615c6d020b7b7402217a4227110ce6b56e60290
123a456b789c <50078004+123a456b789c@users.noreply.github.com>
2022-05-07 12:39:03 +0200

    Fixed split function in edge cases, added file write

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const fs = require("fs")
+const args = require('minimist')(process.argv.slice(2));
 const words = fs.readFileSync(__dirname + "/words.txt", 'utf-8').split("\r\n")
+const writePath = __dirname + "/buffer.bin";
 
 var specialChars = ["!","?",".",","]
-var text = "What do you want? I want to cook lunch!"
 
-//TODO fix split in edge cases
+function writeToFile(buffer) {
+    fs.createWriteStream(writePath).write(Buffer.from(buffer));
+}
+
+function readFromFile(file) {
+    const buf = fs.readFileSync('buffer.bin');
+    //return Buffer.from(buf.toString('hex'), "hex")
+    decode(Buffer.from(buf.toString("hex"), "hex"))
+}
+
 function encode(text) {
     for(i = 0; i < specialChars.length; i++) {
         if (text.includes(specialChars[i]) == true ) {
-            text.replace(specialChars[i], "ii")
+            text = text.replace(specialChars[i], " " + specialChars[i])
         }
     }
     var arr = text.split(" ")
-    console.log(text)
     var buffer = new ArrayBuffer(arr.length * 4)
     const bufferView = new Int32Array(buffer)
     for(var i = 0; i < arr.length; i++) {
@@ -22,9 +31,6 @@ function encode(text) {
    }
    return buffer
 }
-
-console.log(encode(text))
-console.log(decode(encode(text)))
 
 function decode(buffer) {
     var text = ""
@@ -41,3 +47,6 @@ function decode(buffer) {
     }
     return text
 }
+
+writeToFile(encode("Life is the most beautiful thing I have ever experienced."))
+console.log(decode(encode("Life is the most beautiful thing I have ever experienced.")))
